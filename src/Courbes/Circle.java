521bb4e7603d4bf04a8845d8f4ee09017f360667
Amir <benslima@polytech.unice.fr>
2013-12-20 14:28:22 +0100

    Amelioration de drawCirlcles

--- src/Courbes/Circle.java
+++ src/Courbes/Circle.java
@@ -1,41 +1,59 @@
 package Courbes;
 
-/**
- * methods/CirclePanel.java - Component to draw circles.
- * This class functions as a GUI component, and can be added to a layout.
- * @version 22 June 1998, revised July 1999,  2002-02-07 JPanel
- * @author Fred Swartz
- */
-
 import java.awt.*;
 import javax.swing.*;
 
 public class Circle extends JPanel {
 
-	// =========================================== constructor
 	public Circle() {
 		setPreferredSize(new Dimension(100, 100));
 		setBackground(Color.white);
-	}// end constructor
+	}
 
-	// =========================================== paintComponent
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		drawCircles(g, 23, 200, 200, 200);
-	}// end paintComponent
+		drawCircles2(g, 3, this.getWidth() / 2, this.getHeight() / 2, 50);
+	}
 
-	// ========================================== drawCircle
-	// Convenience method to draw from center with radius
 	public void drawCircle(Graphics cg, int xCenter, int yCenter, int r) {
 		cg.drawOval(xCenter - r, yCenter - r, 2 * r, 2 * r);
-	}// end drawCircle
+	}
 
 	public void drawCircles(Graphics cg, int k, int xCenter, int yCenter, int r) {
-		if(k!=0){
+		if (k == 0) {
 			drawCircle(cg, xCenter, yCenter, r);
-
-			drawCircles(cg, k-1, (int) (xCenter + 1.5 * r), yCenter, r / 2);
-			drawCircles(cg, k-1, xCenter, (int) (yCenter + 1.5 * r), r / 2);
+		} else {
+			drawCircle(cg, xCenter, yCenter, r);
+			drawCircles(cg, k - 1, (int) (xCenter + 1.5 * r), yCenter, r / 2);
+			drawCircles(cg, k - 1, xCenter, (int) (yCenter + 1.5 * r), r / 2);
 		}
 	}
-} // end class CirclePanel
+
+	public int sommeRayons(int r, int k) {
+		float res = r;
+		for (int i = 1; i < k; i++) {
+			res += r / (2*i);
+		}
+		return (int) res;
+	}
+
+	public void drawCircles2(Graphics cg, int k, int xCenter, int yCenter, int r) {
+		int xCenterInit = xCenter, yCenterInit = yCenter;
+		if (Math.abs(xCenter - sommeRayons(r, k)) > 0) {
+
+			if (k == 0) {
+				drawCircle(cg, xCenter, yCenter, r);
+			} else {
+				drawCircle(cg, xCenter, yCenter, r);
+				drawCircles2(cg, k - 1, (int) (xCenter + 1.5 * r), yCenter,
+						r / 2);
+				drawCircles2(cg, k - 1, xCenter, (int) (yCenter + 1.5 * r),
+						r / 2);
+				drawCircles2(cg, k - 1, (int) (xCenter - 1.5 * r), yCenter,
+						r / 2);
+				drawCircles2(cg, k - 1, xCenter, (int) (yCenter - 1.5 * r),
+						r / 2);
+			}
+		}
+	}
+}
--- src/Courbes/Main.java
+++ src/Courbes/Main.java
@@ -1,6 +1,9 @@
 package Courbes;
 
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,13 +11,20 @@ import javax.swing.JPanel;
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+		final JFrame frame = new JFrame();
 		Circle circle = new Circle();
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				super.windowClosed(e);
+				frame.dispose();
+				System.exit(0);
+			}
+		});
 		frame.setContentPane(circle);
 		
-		frame.setSize(1000, 1500);
+		frame.setSize(1500, 800);
 		frame.setVisible(true);
-
 	}
 
 }
